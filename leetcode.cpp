b37be1bbbfd949a150fd4bc45df2621db92a6fea
Jiqi Hu <ianhu@JiqideMacBook-Pro.local>
2025-05-06 18:13:16 -0400

    1st try

---
+++ leetcode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    cout << "Try programiz.pro" << endl;
+    return 0;
+}
