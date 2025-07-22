756a54b7dca3145085aa8a2dec83807e06167465
Jiqi Hu <hujiqi1712828578@gmail.com>
2025-07-22 15:35:46 -0400

    2410

---
+++ 2410.cpp
@@ -0,0 +1,58 @@
+#include <iostream>     // Input/output stream operations
+#include <string>       // String manipulation (std::string)
+#include <vector>       // Dynamic arrays (std::vector)
+#include <algorithm>    // Common algorithms (sort(), find(), etc.)
+#include <cmath>        // Mathematical functions (sqrt(), pow(), etc.)
+#include <cstdlib>      // General utility functions (malloc(), free(), rand(), etc.)
+#include <ctime>        // Date and time functions
+#include <fstream>      // File input/output operations
+#include <map>          // Sorted associative containers (std::map)
+#include <set>          // Unique sorted elements (std::set)
+#include <list>         // Doubly linked list (std::list)
+#include <queue>        // Queues (std::queue, std::priority_queue)
+#include <stack>        // Stack container (std::stack)
+#include <tuple>        // Tuple container (std::tuple)
+#include <iterator>     // Iterator utilities
+#include <limits>       // Numeric limits (std::numeric_limits)
+#include <memory>       // Smart pointers (std::unique_ptr, std::shared_ptr)
+#include <type_traits>  // Compile-time type checking and manipulation
+#include <thread>       // Multithreading (std::thread)
+#include <atomic>       // Atomic operations
+#include <condition_variable>  // Synchronization primitives
+#include <exception>    // Exception handling (std::exception)
+#include <stdexcept>    // Standard exceptions (std::runtime_error, std::out_of_range)
+#include <cassert>      // Assertions (assert())
+#include <cctype>       // Character handling functions (isdigit(), tolower(), etc.)
+#include <sstream>      // String stream operations (std::stringstream)
+#include <numeric>      // Numeric operations (accumulate(), iota(), etc.)
+#include <climits>      // For INT_MIN
+
+using namespace std;
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int n = players.size();
+        int m = trainers.size();
+
+        int i = n - 1;
+        int j = m - 1;
+        int ans = 0;
+        while (i >= 0 && j >= 0) {
+            if (players[i] <= trainers[j]) {
+                ans++;
+                i--;
+                j--;
+            } else {
+                i--;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
